2e09bb5a837189281a63f7ef649c6fc5b85d49c3
sachin saha <sachinsaha628@gmailcom>
2025-11-26 10:46:08 +0530

    find smallest element in array

---
+++ Arrays/Q1_find_smallest.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include <climits>
+using namespace std;
+
+int findSmallest(int arr[], int n){
+    int smallest = INT_MAX;
+    for(int i = 0; i<n; i++){
+        if(arr[i]<smallest){
+            smallest = arr[i];
+        }
+    }
+    return smallest;
+}
+
+int main(){
+    int arr[5] = {11,3,55,66,-7};
+    int n = 5;
+    
+    cout<<findSmallest(arr,n)<<endl;
+    
+
+}
