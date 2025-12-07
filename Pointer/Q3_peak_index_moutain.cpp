6898acb058be31ff7625a9ea8891f7dba5c0129b
sachin saha <sachinsaha628@gmail.com>
2025-12-07 23:20:30 +0530

    peak index in mountain array

---
+++ Pointer/Q3_peak_index_moutain.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int peakIndexInMountaininArray(vector<int> A)
+{
+    int st = 1, end = A.size() - 2;
+    while (st <= end)
+    {
+        int mid = st + (end - st)/2;
+        if (A[mid - 1] < A[mid] && A[mid] > A[mid + 1])
+        {
+            return mid;
+        }
+        else if(A[mid - 1] < A[mid]) // right
+        {
+            st = mid + 1;
+        }
+        else // left
+        {
+            end = mid - 1;
+        }
+    }
+}
+
+int main()
+{
+    vector<int> arr = {0,1,0};
+    cout<<peakIndexInMountaininArray(arr);
+}
