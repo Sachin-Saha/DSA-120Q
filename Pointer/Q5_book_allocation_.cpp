ed9467c5bc2e21dcb6df520f9fd579252e537918
sachin saha <sachinsaha628@gmail.com>
2025-12-11 23:58:17 +0530

    book allocation

---
+++ Pointer/Q5_book_allocation_.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+
+
+
+bool isValid(vector<int> &arr, int n, int m, int maxAllowedPages){
+    int students = 1, pages = 0;
+    for(int i=0; i<n; i++){
+        if(arr[i]> maxAllowedPages){
+            return false;
+        }
+        if(pages + arr[i] <= maxAllowedPages)
+    }
+}
+
+
+
+
+
+int allocateBooks(vector<int> &arr, int n, int m){
+    if(m>n){
+        return -1;
+    }
+    int sum = 0;
+    fot(int i=0; i<n; i++){
+        sum += arr[i];
+    }
+
+    int ans = -1;
+    int st = 0, end = sum; 
+
+    while(st<=end){
+        int mid = st + (end-st)/2;
+        if(isValid(arr,n,m,mid)){
+            ans = mid;
+            end = mid -1;
+        }else{
+            st = mid + 1;
+        }
+
+    }
+    return ans;
+}
+
+
+int main(){
+    vector<int> arr = {15,17,20};
+    int n = 4, m= 5;
+
+    cout<<allocateBooks(arr, n, m)<<endl;
+    return 0;
+}
