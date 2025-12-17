f9086561642abc1ad33a7f789ce0306836b19f8e
sachin saha <sachinsaha628@gmail.com>
2025-12-17 10:12:03 +0530

    solve by mapp

--- Arrays/Q12_two_sum_.cpp
+++ Arrays/Q12_two_sum_.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
 #include<vector>
+#include<map>
 using namespace std;
 
+// brute force approach
 vector<int> twoSum(vector<int> &arr, int target){
     int n = arr.size();
     vector<int> ans;
@@ -18,11 +20,29 @@ vector<int> twoSum(vector<int> &arr, int target){
     return ans; // print empty space, if target not found
 }
 
+// better approach using map 
+vector<int> twoSumUsingMap(vector<int> &arr, int target){
+    int n = arr.size();
+    map<int,int>mpp;
+    for(int i = 0; i<n; i++){
+        int first = arr[i];
+        int second = target - first;
+        if(mpp.find(second) != mpp.end()){
+            return {mpp[second], i};
+        }
+        mpp[first] = i;
+    }
+    return {-1,-1};
+}
+
 int main(){
     vector<int> arr = {2,7,11,5};
     int target = 9;
 
-    vector<int> ans = twoSum(arr,target);
-    cout<<ans[0]<<","<<ans[1];
+    vector<int> ans = twoSum(arr,target); // brute force approach
+    vector<int> ans1 = twoSumUsingMap(arr,target); // map approach 
+    cout<<ans[0]<<","<<ans[1]; // ans of brute force approach
+    cout<<endl;
+    cout<<ans1[0]<<","<<ans1[1]; // ans of map approach
 
 }
