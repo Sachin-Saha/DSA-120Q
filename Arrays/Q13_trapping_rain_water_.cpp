703e983a864883ea31e1edaed1a5e2e9272b08b2
sachin saha <sachinsaha628@gmail.com>
2025-12-21 23:29:47 +0530

    trapping rain water

--- Arrays/Q13_trapping_rain_water_.cpp
+++ Arrays/Q13_trapping_rain_water_.cpp
@@ -1,4 +1,9 @@
-  int n = height.size();
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int trap(vector<int>& height){
+    int n = height.size();
     int ans = 0;
     int l = 0, r = n-1;
     int lmax = 0, rmax = 0;
@@ -11,7 +16,15 @@
             l++;
         }else{
             ans +=  rmax - height[r];
+            r--;
         }
     }
 
     return ans;
+}
+
+int main(){
+    vector<int> arr ={0,1,0,2,1,0,1,3,2,1,2,1};
+    int newAns = trap(arr);
+    cout<<newAns;
+}
